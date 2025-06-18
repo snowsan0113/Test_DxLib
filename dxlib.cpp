7340835583ef79048bb7b87c217c9bf0160eaac6
snowsan0113 <egawayuuki0113@gmail.com>
2025-06-18 20:58:37 +0900

    DxLibで円をキーで動かす練習

--- dxlib.cpp
+++ dxlib.cpp
@@ -1,13 +1,48 @@
 #include "DxLib.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
+	ChangeWindowMode(TRUE);
+	SetMainWindowText("DxLibのテストテストテストテスト");
 	if (DxLib_Init() == -1)	{
 		return -1;
 	}
 
-	ChangeWindowMode(TRUE);
-	DrawPixel(320, 240, GetColor(255, 255, 255));
-	WaitKey();
-	DxLib_End();
-	return 0;	
+	bool cmd_mode = false;
+	int mouseX = 0, mouseY = 0;
+	int circleX = 0, circleY = 0;
+	while (true) {
+		ClearDrawScreen();
+		GetMousePoint(&mouseX, &mouseY);
+
+		DrawFormatString(400,400, GetColor(255, 255, 255), "（%d, %d）", mouseX, mouseY);
+
+		if (CheckHitKey(KEY_INPUT_SLASH) == 1) {
+			cmd_mode = true;
+		}
+
+		if (circleX == 0 && circleY == 0) {
+			circleX = mouseX;
+			circleY = mouseY;
+		}
+		if (CheckHitKey(KEY_INPUT_UP)) circleY -= 1;
+		if (CheckHitKey(KEY_INPUT_DOWN)) circleY += 1;
+		if (CheckHitKey(KEY_INPUT_LEFT)) circleX -= 1;
+		if (CheckHitKey(KEY_INPUT_RIGHT)) circleX += 1;
+
+		DrawCircle(circleX, circleY, 10, GetColor(255, 255, 255));
+		DrawFormatString(400, 420, GetColor(255, 0, 0), "図形：（%d, %d）", circleX, circleY);
+
+		ScreenFlip();
+
+		//終了する
+		if (cmd_mode) {
+			DxLib_End();
+			break;
+			return 0;
+
+			if (ProcessMessage() == -1) {
+				break;      
+			}
+		}
+	}
 }
